03f40ca2e91892901bc4a316d83b61e7357aabb6
akhmadnas <32979162+akhmadnas@users.noreply.github.com>
2021-02-24 15:11:37 +0700

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -11,9 +11,9 @@
 | Inventory Database Configuration
 |--------------------------------------------------------------------------
 */
-define( 'DB_HOST', 'localhost' );          // Set database host
-define( 'DB_USER', 'webuser' );             // Set database user
-define( 'DB_PASS', 'p1r4sp' );             // Set database password
+define( 'DB_HOST', 'mysql1' );          // Set database host
+define( 'DB_USER', 'root' );             // Set database user
+define( 'DB_PASS', '' );             // Set database password
 define( 'DB_NAME', 'inventory' );        // Set database name
 
 ?>
